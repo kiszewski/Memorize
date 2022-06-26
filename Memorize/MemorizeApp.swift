15f5828b0845c4da31837f45ca5205a0226c8fe2
Leonardo Kiszewski <kiszewski1999@gmail.com>
2022-06-26 18:28:10 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Leonardo Kiszewski on 26/06/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

f77a3e3af38306274da4ae7fc3ac8db1e5e2582b
Leonardo Kiszewski <kiszewski1999@gmail.com>
2022-06-26 19:42:26 -0300

    First commit memorize

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  Memorize
+//  Shared
 //
 //  Created by Leonardo Kiszewski on 26/06/22.
 //
@@ -9,10 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            ForEach(0..<4) {
+                index in CardView(isFaceUp: true)
+            }
+        }.padding()
+        
+//        Same:
+//        HStack (content: {
+//            ForEach(0..<4, content:  {
+//                index in CardView(isFaceUp: true)
+//            })
+//        }).padding()
     }
 }
+
+
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            if(isFaceUp) {
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke(lineWidth: 3)
+                RoundedRectangle(cornerRadius:8)
+                    .fill(Color.white)
+                Text("👻").font(Font.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius:8)
+            }
+        }.foregroundColor(Color.orange)
+    }
+}
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
